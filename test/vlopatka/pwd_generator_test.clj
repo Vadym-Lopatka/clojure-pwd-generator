9d1c69a5aecbeeb57ad7b379b2d309d2091e0619
vlopatka <vadim.lopatka@gmail.com>
2023-05-13 15:04:55 +0300

    init

---
+++ test/vlopatka/pwd_generator_test.clj
@@ -0,0 +1,7 @@
+(ns vlopatka.pwd-generator-test
+  (:require [clojure.test :refer :all]
+            [vlopatka.pwd-generator :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
