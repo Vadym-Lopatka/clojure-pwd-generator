ad4faa48db4024bbbab1902a72df3432095011d9
vlopatka <vadim.lopatka@gmail.com>
2023-05-13 18:45:51 +0300

    added cli tooling

--- src/vlopatka/pwd_generator.clj
+++ src/vlopatka/pwd_generator.clj
@@ -4,26 +4,27 @@
             [clojure.tools.cli :refer [parse-opts]])
   (:gen-class))
 
-;; (def cli-options
-;;   ;; An option with a required argument
-;;   [["-p" "--port PORT" "Port number"
-;;     :default 80
-;;     :parse-fn #(Integer/parseInt %)
-;;     :validate [#(< 0 % 0x10000) "Must be a number between 0 and 65536"]]
-;;    ;; A non-idempotent option (:default is applied first)
-;;    ["-v" nil "Verbosity level"
-;;     :id :verbosity
-;;     :default 0
-;;     :update-fn inc] ; Prior to 0.4.1, you would have to use:
-;;                    ;; :assoc-fn (fn [m k _] (update-in m [k] inc))
-;;    ;; A boolean option defaulting to nil
-;;    ["-h" "--help"]])
+(def cli-options
+  [["-l" "--length LENGTH" "Password length"
+    :default 12
+    :parse-fn #(Integer/parseInt %)
+    :validate [#(< 0 % 100) "Must be a number between 0 and 100"]] 
+   ["-h" "--help"]])
+
+(defn generate-new-password [length]
+  (let [pwd (generate-password length)]
+    (copy pwd)
+    (println pwd)))
 
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (copy (generate-password 9)))
-
-(-main)
+  (let [arguments (parse-opts args cli-options)
+        options (:options arguments)
+        summary (:summary arguments)]
+    
+    (if (:help options)
+      (println summary)
+      (generate-new-password (:length options)))))
 
