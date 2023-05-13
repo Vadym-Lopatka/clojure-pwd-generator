6fa86689b47efe1242de6a07c33e406fc321a41f
vlopatka <vadim.lopatka@gmail.com>
2023-05-13 15:25:46 +0300

    added tool.cli dep

--- src/vlopatka/pwd_generator.clj
+++ src/vlopatka/pwd_generator.clj
@@ -1,12 +1,9 @@
 (ns vlopatka.pwd-generator
   (:gen-class))
 
-(defn greet
-  "Callable entry point to the application."
-  [data]
-  (println (str "Hello, " (or (:name data) "World") "!")))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (greet {:name (first args)}))
+  "hello")
+
